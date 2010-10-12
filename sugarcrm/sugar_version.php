4a028fe29350850f1a69cfc8a96552b8de39cf2c
engsvnbuild <engsvnbuild@cd3d3a3c-4e5e-4c0c-b5be-c9cbe94fe7b3>
2010-10-12 04:11:39 +0000

    automated 6.1.0RC1 to trunk sync

--- sugarcrm/sugar_version.php
+++ sugarcrm/sugar_version.php
@@ -40,7 +40,7 @@
 $sugar_version      = '6.1.0RC1';
 $sugar_db_version   = '6.1.0RC1';
 $sugar_flavor       = 'CE';
-$sugar_build		= '5222';
-$sugar_timestamp    = '2010-10-08 08:20am';
+$sugar_build		= '5223';
+$sugar_timestamp    = '2010-10-11 08:57am';
 
 ?>
